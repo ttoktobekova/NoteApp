19f0a28f0991ec8437d60d33fa7b6bb7abac960c
ttoktobekova <gul775653@gmail.com>
2024-06-04 15:56:53 +0600

    OnBoard create

---
+++ app/src/main/java/com/example/noteapp/UI/fragments/note/NoteFragment.kt
@@ -0,0 +1,25 @@
+package com.example.noteapp.UI.fragments.note
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.noteapp.databinding.FragmentNoteBinding
+
+class NoteFragment : Fragment() {
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
